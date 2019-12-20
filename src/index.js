748164b93efdeece11b28748f637cf20bf17e030
Ladanov Nikolay <ladanov_n@mail.ru>
2019-12-20 10:20:15 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import "./style/index.scss";
+import "./assets/images/image-placeholder-inverse.svg";
+import "./assets/images/image-placeholder-primary.svg";
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.addEventListener('click', (e) => {
+        for (const item of e.path) {
+            if (item.classList) {
+                if (item.classList.contains('history__transaction')) {
+                    if (item.classList.contains('history__transaction_hide')) {
+                        item.classList.remove('history__transaction_hide');
+                    } else {
+                        item.classList.add('history__transaction_hide');
+                    }
+                    break;
+                }
+            }
+        }
+
+        if (e.target.classList.contains('onoffswitch__button')) {
+            const switcher = document.getElementsByClassName('onoffswitch')[0];
+            const page = document.getElementsByClassName('page')[0];
+
+            if (switcher.classList.contains("onoffswitch_checked")) {
+                switcher.classList.remove("onoffswitch_checked");
+            } else {
+                switcher.classList.add("onoffswitch_checked");
+            }
+
+            if (page.classList.contains("theme_color_project-default")) {
+                page.classList.remove("theme_color_project-default");
+                page.classList.add("theme_color_project-inverse");
+            } else {
+                page.classList.remove("theme_color_project-inverse");
+                page.classList.add("theme_color_project-default");
+            }
+        }
+    });
+});
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'script.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: ['babel-loader',
+                {
+                    loader: 'eslint-loader',
+                    options: {
+                        fix: true,
+                    },
+                }],
+        }, {
+            test: /\.pug$/,
+            use: ['html-loader?attrs=false', 'pug-html-loader']
+        },
+        {
+            test: /\.(css|scss)$/,
+            use: [
+                {
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        publicPath: './static',
+                        minimize: true,
+                    },
+                },
+                { loader: 'css-loader' },
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        plugins: [
+                            autoprefixer({
+                                overrideBrowserslist: ['last 2 versions'],
+                            }),
+                        ],
+                        sourceMap: true,
+                    },
+                },
+                { loader: 'sass-loader' },
+            ],
+        }, {
+            test: /\.(png|jpe?g|gif|svg)$/i,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        name: 'img/[name].[ext]',
+                    },
+                },
+            ],
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.pug',
+            inject: false
+        })
+    ]
+};
+module.exports = config;
